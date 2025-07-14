a45a86a5cf46c491393ab989855b1bd85c0a07cd
skilltrik <badruever@gmail.com>
2025-07-14 14:58:17 +0300

    Delete check.py

--- check.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime, timezone, timedelta
-
-timezone_offset = 32400
-custom_timezone = timezone(timedelta(seconds=timezone_offset))
-sunrise_utc = datetime.fromtimestamp(1751916731, tz=timezone.utc)
-sunrise_local = sunrise_utc.astimezone(custom_timezone).strftime("%Y-%m-%d %H:%M:%S")
-print(sunrise_local)
